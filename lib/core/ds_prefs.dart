67186a6c5677ffe5c2b69b4425da141a2267dfe3
Alexey Tushurashvili <altush@yandex.ru>
2023-07-17 09:58:37 +0300

    DSPrefs now is a ChangeNotifier

--- lib/core/ds_prefs.dart
+++ lib/core/ds_prefs.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-abstract class DSPrefs {
+abstract class DSPrefs extends ChangeNotifier {
   SharedPreferences? _prefs;
   SharedPreferences get internal => _prefs!;
 
@@ -22,14 +23,55 @@ abstract class DSPrefs {
     _prefs = await SharedPreferences.getInstance();
   }
 
+  void notifyAsync() {
+    Timer.run(() {
+      notifyListeners();
+    });
+  }
+
+  @protected
+  void setBool(String key, bool value) {
+    internal.setBool(key, value);
+    notifyAsync();
+  }
+
+  @protected
+  void setInt(String key, int value) {
+    internal.setInt(key, value);
+    notifyAsync();
+  }
+
+  @protected
+  void setDouble(String key, double value) {
+    internal.setDouble(key, value);
+    notifyAsync();
+  }
+
+  @protected
+  void setString(String key, String value) {
+    internal.setString(key, value);
+    notifyAsync();
+  }
+
+  @protected
+  void setStringList(String key, List<String> value) {
+    internal.setStringList(key, value);
+    notifyAsync();
+  }
+
+  @protected
+  void remove(String key) {
+    internal.remove(key);
+    notifyAsync();
+  }
+
   int getSessionId() => internal.getInt('app_session_id') ?? 0;
-  void setSessionId(int value) => internal.setInt('app_session_id', value);
+  void setSessionId(int value) => setInt('app_session_id', value);
 
   DateTime getAppLastUsed() =>
       DateTime.fromMillisecondsSinceEpoch((internal.getInt('app_last_used') ?? 0) * 1000);
-  void setAppLastUsed(DateTime value) =>
-      unawaited(internal.setInt('app_last_used', value.millisecondsSinceEpoch ~/ 1000));
+  void setAppLastUsed(DateTime value) => setInt('app_last_used', value.millisecondsSinceEpoch ~/ 1000);
 
   bool isYandexDeviceIdSent() => internal.getBool('yandexDeviceIdSent') ?? false;
-  void setYandexDeviceIdSent(bool value) => internal.setBool('yandexDeviceIdSent', value);
+  void setYandexDeviceIdSent(bool value) => setBool('yandexDeviceIdSent', value);
 }
